441f6cb17d2ff5ced0cf96c3a3cea5a4c7cd0349
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2021-10-26 07:47:06 +0530

    Added discount

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,7 @@ class product(models.Model):
     id= models.CharField(max_length=20,primary_key=True, serialize=False, verbose_name='Product ID')
     product_name = models.CharField(max_length=120)
     product_price = models.IntegerField()
+    before_discount_price = models.FloatField()
     product_description = models.TextField()
     product_pic = models.ImageField(upload_to="product-images/")
 
